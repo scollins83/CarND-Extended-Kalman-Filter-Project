e84a6b3c4147426be06f87219c8744f86e69b63b
Sara Collins <datadivallc@gmail.com>
2018-04-05 14:46:23 -0500

    Added initial F transition matrix, and Q transform to FusionEKF. Also had to add floats for the noise variables to the header.  In kalman_filter.cpp, I added the predict function contents.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -38,7 +38,15 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
-  // Determine which and where to add H and R to the kalman filter. Ok, decided to try it down in the initialization.
+  // Adding initial transition matrix
+  ekf_.F_ = MatrixXd(4, 4);
+  ekf_.F_ << 1, 0, 1, 0,
+             0, 1, 0, 1,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
+
+  noise_ax = 9;
+  noise_ay = 9;
 
 
 }
@@ -103,6 +111,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     is_initialized_ = true;
     return;
   }
+
+  // Compute elapsed time between current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+
+  // Modify F matrix so time is integrated
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+
+  // Set the process covariance matrix Q
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+             0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+             dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+             0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
   /*****************************************************************************
    *  Prediction
